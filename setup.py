a763e7a3c284dbcfef8ee7745e9ee7d54b07244b
alex <alexyakimchyk@gmail.com>
2020-06-01 00:21:45 +0300

    added examples

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='graphframes',
       version='0.8.0',
-      packages=['graphframes', 'graphframes.lib'],
+      packages=['graphframes', 'graphframes.lib', 'graphframes.examples'],
       install_requires=[
           'nose==1.3.3',
           'numpy>=1.7'
